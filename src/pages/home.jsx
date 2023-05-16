be8a6f1bea2c525386b526c332d6a77531613759
sai shravan <94429984+shravan81@users.noreply.github.com>
2023-05-16 09:52:33 +0530

    feat: Add basic elements to Home component
    - Create divs, p, and links in the Home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,47 @@
 import '../styles/pages/home.scss'
 
 const Home = () => {
-  // Component logic
+  return (
+    <div>
+      <div className='left'>
+        <label htmlFor=''>Email:</label>
+        <input type='text' />
+        <label htmlFor=''>Password:</label>
+        <input type='password' />
+      </div>
 
-  return <h1>hi</h1>
+      <div className='right'>
+        <p>Welcome to Thefacebook!</p>
+        <h1>[ Welcome to Thefacebook ]</h1>
+        <p>
+          Thefacebook is an online directory that connects people through social
+          networks at colleges
+        </p>
+        <p>
+          We have recently opened up Thefacebook at the following schools:
+          Arizona • Arizona State • Bryn Mawr • CU Boulder • Drexel • Loyola
+          Marymount • Miami • Mt. Holoyoke • Trinity College
+        </p>
+        <p>
+          For a complete list of supported schools, click <a href='/'>here</a>
+        </p>
+        <p>Your facebook is limited to your won college or university</p>
+        <ul>
+          <p>You can use TheFacebook to:</p>
+          <li>Search for people at your school</li>
+          <li>find out who is in your classes</li>
+          <li>Look up your friends' friends</li>
+          <li>See a visualization of your social network</li>
+        </ul>
+        <p>
+          To get started, click below ot register. If you have already
+          registered, you can log in.
+        </p>
+        <button>Register</button>
+        <button>Login</button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
