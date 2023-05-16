d03527953aab7a02d01034e3b48f819ef5e112be
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-16 08:56:01 +0000

    feat: add styles to home pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,43 +2,56 @@ import '../styles/pages/home.scss'
 
 const Home = () => {
   return (
-    <div>
-      <div className='left'>
-        <label htmlFor=''>Email:</label>
-        <input type='text' />
-        <label htmlFor=''>Password:</label>
-        <input type='password' />
-      </div>
+    <div className='home'>
+      <div className='wrapper'>
+        <div className='left'>
+          <label htmlFor=''>Email:</label>
+          <input type='text' />
+          <label htmlFor=''>Password:</label>
+          <input type='password' />
+          <div>
+            <button>Register</button>
+            <button>Login</button>
+          </div>
+        </div>
 
-      <div className='right'>
-        <p>Welcome to Thefacebook!</p>
-        <h1>[ Welcome to Thefacebook ]</h1>
-        <p>
-          Thefacebook is an online directory that connects people through social
-          networks at colleges
-        </p>
-        <p>
-          We have recently opened up Thefacebook at the following schools:
-          Arizona • Arizona State • Bryn Mawr • CU Boulder • Drexel • Loyola
-          Marymount • Miami • Mt. Holoyoke • Trinity College
-        </p>
-        <p>
-          For a complete list of supported schools, click <a href='/'>here</a>
-        </p>
-        <p>Your facebook is limited to your won college or university</p>
-        <ul>
-          <p>You can use TheFacebook to:</p>
-          <li>Search for people at your school</li>
-          <li>find out who is in your classes</li>
-          <li>Look up your friends' friends</li>
-          <li>See a visualization of your social network</li>
-        </ul>
-        <p>
-          To get started, click below ot register. If you have already
-          registered, you can log in.
-        </p>
-        <button>Register</button>
-        <button>Login</button>
+        <div className='right'>
+          <p className='top'>Welcome to Thefacebook!</p>
+          <div className='main'>
+            <h1>[ Welcome to Thefacebook ]</h1>
+            <p>
+              Thefacebook is an online directory that connects people through
+              social networks at colleges
+            </p>
+            <p>
+              We have recently opened up Thefacebook at the following schools:
+            </p>
+            <p className='schools'>
+              Arizona • Arizona State • Bryn Mawr • CU Boulder • Drexel • Loyola
+              Marymount • Miami • Mt. Holoyoke • Trinity College
+            </p>
+            <p>
+              For a complete list of supported schools, click{' '}
+              <a href='/'>here</a>
+            </p>
+            <p>Your facebook is limited to your won college or university</p>
+            <p>You can use TheFacebook to:</p>
+            <ul>
+              <li>Search for people at your school</li>
+              <li>find out who is in your classes</li>
+              <li>Look up your friends' friends</li>
+              <li>See a visualization of your social network</li>
+            </ul>
+            <p>
+              To get started, click below to register. If you have already
+              registered, you can log in.
+            </p>
+            <div className='buttons'>
+              <button>Register</button>
+              <button>Login</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
