9fcc1119a89d25783682d8fe6a229ab1fcbd47be
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-16 08:05:25 +0000

    feat: add styles to Layout
    -Add a maximum width
    -Padding for bounds

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Header from './header'
 import Footer from './footer'
+import '../styles/components/layout.scss'
 
 const Layout = () => {
   return (
-    <>
+    <div className='layout'>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
