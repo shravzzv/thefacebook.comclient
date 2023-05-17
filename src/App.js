e98b426e37e853b28229195f336d2e8888e62e91
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-17 07:25:39 +0000

    feat: add footer routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
 import About from './pages/about'
+import Contact from './pages/contact'
+import Faq from './pages/faq'
+import Media from './pages/media'
+import Terms from './pages/terms'
+import Policy from './pages/policy'
 import NoMatch from './pages/noMatch'
 
 function App() {
@@ -14,9 +19,14 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='about' element={<About />} />
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='faq' element={<Faq />} />
+          <Route path='media' element={<Media />} />
+          <Route path='terms' element={<Terms />} />
+          <Route path='policy' element={<Policy />} />
           <Route path='*' element={<NoMatch />} />
         </Route>
       </Routes>
