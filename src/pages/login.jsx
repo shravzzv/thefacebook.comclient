1f8e52c6fbf9dd16e38739691d789511f092fec4
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-17 03:23:18 +0000

    feat: add styles to login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,31 @@
 import '../styles/pages/login.scss'
+import AuthBox from "../components/authBox"
 
 const Login = () => {
-  // Component logic
+  return <div className='login'>
+    <div className='wrapper'>
+      <AuthBox />
 
-  return <h1>hi</h1>
+      <div className='content'>
+        <h1>[ Login ]</h1>
+        <form>
+          <div className='form-group'>
+            <label>Email:</label>
+            <input type='email' />
+          </div>
+          <div className='form-group'>
+            <label>Password:</label>
+            <input type='password' />
+          </div>
+          <div className="buttons">
+            <button type='submit'>Login</button>
+            <button>Register</button>
+          </div>
+        </form>
+        <p>If you have forgotten your password, click <a href="/login">here</a> to reset it.</p>
+      </div>
+    </div>
+  </div>
 }
 
 export default Login
