9490f9ffd8d138d3434899b657f4472a3e71bf22
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-17 15:00:31 +0000

    feat: Add AuthProvider to App.js

--- src/App.js
+++ src/App.js
@@ -12,25 +12,28 @@ import Media from './pages/media'
 import Terms from './pages/terms'
 import Policy from './pages/policy'
 import NoMatch from './pages/noMatch'
+import { AuthProvider } from './context/auth'
 
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='faq' element={<Faq />} />
-          <Route path='media' element={<Media />} />
-          <Route path='terms' element={<Terms />} />
-          <Route path='policy' element={<Policy />} />
-          <Route path='*' element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='faq' element={<Faq />} />
+            <Route path='media' element={<Media />} />
+            <Route path='terms' element={<Terms />} />
+            <Route path='policy' element={<Policy />} />
+            <Route path='*' element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
+    </AuthProvider>
   )
 }
 
