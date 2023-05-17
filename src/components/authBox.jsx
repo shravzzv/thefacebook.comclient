e6884e6e82feb67bc9deba73e5bfd5f2a2eaff9b
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-17 09:51:25 +0000

    feat: add state to authBox component

--- src/components/authBox.jsx
+++ src/components/authBox.jsx
@@ -1,18 +1,34 @@
-import '../styles/components/authBox.scss'
+import { useState } from 'react';
+import '../styles/components/authBox.scss';
+import { useAuthDispatch } from '../context/auth';
 
 const AuthBox = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useAuthDispatch()
+
+  const handleRegister = () => {
+    // Register logic
+    dispatch({ email, password })
+  };
+
+  const handleLogin = () => {
+    // Login logic
+    dispatch({ email, password })
+  };
+
   return (
     <div className='authBox'>
-      <label htmlFor=''>Email:</label>
-      <input type='text' />
-      <label htmlFor=''>Password:</label>
-      <input type='password' />
+      <label htmlFor='email'>Email:</label>
+      <input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <label htmlFor='password'>Password:</label>
+      <input type='password' id='password' autoComplete='true' value={password} onChange={e => setPassword(e.target.value)} />
       <div>
-        <button>Register</button>
-        <button>Login</button>
+        <button onClick={handleRegister}>Register</button>
+        <button onClick={handleLogin}>Login</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthBox
+export default AuthBox;
