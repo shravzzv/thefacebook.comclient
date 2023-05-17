b961ecb3d974e762fe516042e33100dd3a92e3b3
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-17 07:05:53 +0000

    fix: update about page details

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,31 @@
 import '../styles/pages/about.scss'
+import AuthBox from '../components/authBox'
 
 const About = () => {
-  // Component logic
+  return (
+    <div className='about'>
+      <div className='wrapper'>
+        <AuthBox />
 
-  return <h1>hi</h1>
+        <div className='content'>
+          <h1>[ About ]</h1>
+          <div className='card'>
+            <p className='title'>The Project</p>
+            <div className='info'>
+              <p>Thefacebook is an online directory that connects people through social networks at colleges and universities.</p>
+            </div>
+          </div>
+          <div className="card">
+            <p className="title">The People</p>
+            <div className="info">
+              <p><a href="/contact">Sai Shravan</a> The person behind this project</p>
+            </div>
+          </div>
+          <button>Home</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
