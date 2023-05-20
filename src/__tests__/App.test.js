1cf5dcda4b4010557e6aa42f4bcbfbb12f05fa71
sai shravan <94429984+shravan81@users.noreply.github.com>
2023-05-20 09:03:03 +0530

    feat: add tests using jest

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders the app component', () => {
+  render(<App />);
+  const appElement = screen.getByText(/Hello, React/);
+  expect(appElement).toBeInTheDocument();
+});
