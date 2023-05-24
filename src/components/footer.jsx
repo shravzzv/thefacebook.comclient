2032195fb59927a47e82d3ae5762b531a5280a1f
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-24 05:18:34 +0000

    fix: create /profile route link in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,6 +12,7 @@ const Footer = () => {
           <Link to='/media'>advertise</Link>
           <Link to='/terms'>terms</Link>
           <Link to='/policy'>privacy</Link>
+          <Link to='/profile'>test profile</Link>
         </div>
         <div className='legal'>
           <p>a Sai Shravan production</p>
