2000646b15629b2b45c83f90f7015a1ae1cc8ca9
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-26 15:29:43 +0000

    feat: Add registration functionality with API call to Register page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 import '../styles/pages/register.scss';
 import AuthBox from '../components/authBox.jsx';
 import { Link } from 'react-router-dom';
+import { useAuthDispatch } from '../context/auth';
+import { registerUser } from '../api/auth';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState('')
+  const [terms, setTerms] = useState(false)
+  const dispatch = useAuthDispatch()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      if (password.length < 8) {
+        alert('Password must be at least 8 characters long.')
+        return;
+      }
+
+      const data = await registerUser(email, password, name, status);
+      if (data.error) {
+        alert(data.error);
+      } else {
+        dispatch({
+          type: 'USER_REGISTERED',
+          payload: data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='register'>
       <div className='wrapper'>
@@ -17,37 +48,39 @@ const Register = () => {
               below. You will have a chance to enter additional information and
               submit a picture once you have registered.
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className='form-group'>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' id='name' />
+                <input type='text' value={name} onChange={e => setName(e.target.value)} required />
               </div>
               <div className='form-group'>
                 <label htmlFor='status'>Status:</label>
-                <select id='status'>
-                  <option value='option1'>Student (full time)</option>
-                  <option value='option2'>Student (summer)</option>
-                  <option value='option3'>Grad student</option>
-                  <option value='option4'>Faculty</option>
-                  <option value='option5'>Alumnus/Alumna</option>
+                <select id='status' value={status} onChange={e => setStatus(e.target.value)} required>
+                  <option value='' disabled>Select status</option>
+                  <option value='student(full time)'>Student (full time)</option>
+                  <option value='student(summer)'>Student (summer)</option>
+                  <option value='grad student'>Grad student</option>
+                  <option value='faculty'>Faculty</option>
+                  <option value='alumni'>Alumnus/Alumna</option>
+                  <option value='staff'>Staff</option>
+                  <option value='other'>Other</option>
                 </select>
               </div>
               <div className='form-group'>
                 <label htmlFor='email'>Email (school):</label>
-                <input type='email' id='email' />
+                <input type='email' value={email} onChange={e => setEmail(e.target.value)} required />
               </div>
               <div className='form-group'>
                 <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' />
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)} required />
               </div>
-              <div className='form-group'>
+              <div>
                 <label htmlFor='terms' className="check" >
-                  <input type='checkbox' id='terms' />
-                  I have read and understood the{' '}
-                  <Link to='/register'>Terms of Use</Link>, and I agree to them.
+                  <input type='checkbox' id="terms" checked={terms} onChange={e => setTerms(e.target.checked)} required />
+                  I have read and agree to the <Link to='/terms'>Terms of Use</Link>.
                 </label>
               </div>
-              <button type='submit'>Register Now!</button>
+              <button type='submit' disabled={!terms}>Register Now!</button>
             </form>
           </div>
         </div>
