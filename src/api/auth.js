b8a43f165065f68250381b1c9ec14972258931f7
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-26 10:05:42 +0000

    chore: create api folder to move api calls into

---
+++ src/api/auth.js
@@ -0,0 +1,35 @@
+const url = 'http://localhost:5000/api';
+
+export const registerUser = async (email, password) => {
+  try {
+    const response = await fetch(`${url}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error occurred during registration.');
+  }
+};
+
+export const loginUser = async (email, password) => {
+  try {
+    const response = await fetch(`${url}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error occurred during login.');
+  }
+};
