6b441e0771e47d1b4bce9463477fc82c32aee319
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-26 15:14:36 +0000

    feat: add auth to login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,25 +1,48 @@
+import { useState } from 'react';
 import '../styles/pages/login.scss'
 import AuthBox from "../components/authBox"
+import { useAuthDispatch } from '../context/auth';
+import { loginUser } from '../api/auth';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useAuthDispatch()
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault()
+      const data = await loginUser(email, password);
+      if (data.error) {
+        alert(data.error);
+      } else {
+        dispatch({
+          type: 'USER_LOGGED_IN',
+          payload: data,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div className='login'>
     <div className='wrapper'>
       <AuthBox />
 
       <div className='content'>
         <h1>[ Login ]</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='form-group'>
             <label>Email:</label>
-            <input type='email' />
+            <input type='email' value={email} onChange={e=>setEmail(e.target.value)} required />
           </div>
           <div className='form-group'>
             <label>Password:</label>
-            <input type='password' />
+            <input type='password' value={password} onChange={e=>setPassword(e.target.value)} required autoComplete='true' />
           </div>
           <div className="buttons">
             <button type='submit'>Login</button>
-            <button>Register</button>
           </div>
         </form>
         <p>If you have forgotten your password, click <a href="/login">here</a> to reset it.</p>
