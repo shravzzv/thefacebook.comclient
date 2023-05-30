512598d1bfc5ba74205275a49207eb884a36591c
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-05-30 04:45:45 +0000

    chore: move AuthProvider to index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.scss'
 import Layout from './components/Layout'
@@ -14,30 +14,28 @@ import Policy from './pages/policy'
 import Profile from './pages/profile'
 import NoMatch from './pages/noMatch'
 import Onboarding from './pages/onboarding'
-import { AuthProvider } from './context/auth'
 
 function App() {
+  const auth = false
   return (
-    <AuthProvider>
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-            <Route path='about' element={<About />} />
-            <Route path='contact' element={<Contact />} />
-            <Route path='faq' element={<Faq />} />
-            <Route path='media' element={<Media />} />
-            <Route path='terms' element={<Terms />} />
-            <Route path='policy' element={<Policy />} />
-            <Route path='profile' element={<Profile />} />
-            <Route path='onboarding' element={<Onboarding />} />
-            <Route path='*' element={<NoMatch />} />
-          </Route>
-        </Routes>
-      </div>
-    </AuthProvider>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='faq' element={<Faq />} />
+          <Route path='media' element={<Media />} />
+          <Route path='terms' element={<Terms />} />
+          <Route path='policy' element={<Policy />} />
+          <Route path='profile' element={!auth ? <Navigate to="/login"/> : <Profile />} />
+          <Route path='onboarding' element={!auth ? <Navigate to="/login"/> :<Onboarding />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 
 import './index.scss'
 import App from './App'
+import { AuthProvider } from './context/auth'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
