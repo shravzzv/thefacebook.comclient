bdb59c391fada854f9803b59e534313f855f4b97
Sai Shravan <94429984+shravzzv@users.noreply.github.com>
2023-06-06 22:22:01 +0530

    Update auth.js to use new server url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,9 @@
-const url = 'http://localhost:5000/api'
+// const url = 'http://localhost:5000/api'
+const url = 'https://thefacebook-comserver.vercel.app'
 
 export const registerUser = async (credentials) => {
   try {
-    const response = await fetch(`${url}/auth/register`, {
+    const response = await fetch(`${url}/api/auth/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -19,7 +20,7 @@ export const registerUser = async (credentials) => {
 
 export const loginUser = async (email, password) => {
   try {
-    const response = await fetch(`${url}/auth/login`, {
+    const response = await fetch(`${url}/api/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
